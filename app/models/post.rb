5b10892dd2abf456bb6017934fde0747561e9a01
Whiting <whitinghtennis@gmail.com>
2021-08-31 10:27:49 -0700

    commit tree bec57cbd11578b42f67ffd5f0eadb1da1aee87e8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :clickbait?
+    # validates :title, inclusion: { in: ["Won't Believe", "Secret", "Top [number]", "Guess"] }
+
+
+    def clickbait?
+        unless title && title.match(/(Won't Believe)|(Top \d)|(Secret)|(Guess)/)
+         errors.add(:title, "not clickbait")
+        end
+    end 
+
 end
